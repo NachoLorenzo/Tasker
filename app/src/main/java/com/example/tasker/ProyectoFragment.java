c1576a4b05919a22ed583c40068f8ed52fb29b43
NachoLorenzo <nlorenzow@gmail.com>
2019-05-13 12:26:57 +0200

    Creado Floating Action Button desplegable con animaciones para añadir Usuarios y Proyectos en ProyectoFragment

--- app/src/main/java/com/example/tasker/ProyectoFragment.java
+++ app/src/main/java/com/example/tasker/ProyectoFragment.java
@@ -4,17 +4,23 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Spinner;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -31,10 +37,20 @@ public class ProyectoFragment extends Fragment {
     private Spinner spinnerProyecto;
     private ComunicaProyectoConActivity mListener;
     private DatabaseReference referenceProyecto;
-    private FloatingActionButton añadirProyecto;
+
+    private FloatingActionButton añadir, añadirProyecto, añadirUsuario;
+    Animation fabOpen, fabClose, rotarForward, rotarBackward;
+    boolean isOpen = false;
+
     private Proyecto proyecto;
     private Fragment fNuevoProyecto;
     private FragmentTransaction ft;
+    private FragmentManager fm;
+    private ListView listaUsuarios, listaTareas;
+    private ArrayList<Usuario> arrayUsuarios;
+    private ArrayList<Tarea> arrayTareas;
+    private ArrayAdapter<Usuario> adapterListaUsuarios;
+    private ArrayAdapter<Tarea> adapterListaTareas;
 
     public ProyectoFragment() {
         // Required empty public constructor
@@ -57,22 +73,78 @@ public class ProyectoFragment extends Fragment {
         View v;
         v = inflater.inflate(R.layout.fragment_proyecto, container, false);
 
-        añadirProyecto = v.findViewById(R.id.añadir_proyecto);
-
+        //FIREBASE
         referenceProyecto = FirebaseDatabase.getInstance().getReference();
+        //SPINNER PARA SELECCIONAR PROYECTO
+        spinnerProyecto = (Spinner) v.findViewById(R.id.spinnerProyecto);
+
+        //LISTAS Y ADAPTERS PARA AÑADIR A LOS FLOATING ACTION BUTTON
+        listaUsuarios = v.findViewById(R.id.lista_usuarios);
+        listaTareas = v.findViewById(R.id.lista_tareas);
+        registerForContextMenu(listaUsuarios);
+        registerForContextMenu(listaTareas);
+
+        arrayUsuarios = new ArrayList<Usuario>();
+        arrayTareas = new ArrayList<Tarea>();
+
+        adapterListaUsuarios = new ArrayAdapter<Usuario>(getActivity(), android.R.layout.simple_list_item_1, arrayUsuarios);
+        adapterListaTareas = new ArrayAdapter<Tarea>(getActivity(), android.R.layout.simple_list_item_1, arrayTareas);
+
+        //BLOQUE DE FLOATING ACTION BUTTON
+        añadir = (FloatingActionButton) v.findViewById(R.id.añadir);
+        añadirProyecto = (FloatingActionButton) v.findViewById(R.id.añadir_proyecto);
+        añadirUsuario = (FloatingActionButton) v.findViewById(R.id.añadir_usuario);
+
+        //ANIMACIONES DEL FLOATING ACTION BUTTON
+        fabOpen = AnimationUtils.loadAnimation(getActivity(), R.anim.fab_open);
+        fabClose = AnimationUtils.loadAnimation(getActivity(), R.anim.fab_close);
+        rotarForward = AnimationUtils.loadAnimation(getActivity(), R.anim.rotar_forward);
+        rotarBackward = AnimationUtils.loadAnimation(getActivity(), R.anim.rotar_backward);
+
+        añadir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animarFab();
+            }
+        });
+
         añadirProyecto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                fNuevoProyecto = new NuevoProyectoFragment().newInstance(" "," ");
-                FragmentManager fnp = getFragmentManager();
-                ft = fnp.beginTransaction();
+                fNuevoProyecto = new NuevoProyectoFragment().newInstance("","");
+                fm = getFragmentManager();
+                ft = fm.beginTransaction();
                 ft.replace(R.id.fr_contenido_ppal, fNuevoProyecto);
                 ft.commit();
             }
         });
 
-        spinnerProyecto = (Spinner) v.findViewById(R.id.spinnerProyecto);
+        añadirUsuario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v, "Añadir usuario", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+            }
+        });
+
         return v;
+    }
+
+    private void animarFab(){
+        if(isOpen){
+            añadir.startAnimation(rotarForward);
+            añadirProyecto.startAnimation(fabClose);
+            añadirUsuario.startAnimation(fabClose);
+            añadirProyecto.setClickable(false);
+            añadirUsuario.setClickable(false);
+            isOpen=false;
+        }else{
+            añadir.startAnimation(rotarBackward);
+            añadirProyecto.startAnimation(fabOpen);
+            añadirUsuario.startAnimation(fabOpen);
+            añadirProyecto.setClickable(true);
+            añadirUsuario.setClickable(true);
+            isOpen=true;
+        }
     }
 
     // TODO: Rename method, update argument and hook method into UI event
---
+++ app/src/main/java/com/example/tasker/Tarea.java
@@ -0,0 +1,17 @@
+package com.example.tasker;
+
+public class Tarea {
+
+    String nombre, id;
+
+    public Tarea(String nombre, String id) {
+        this.nombre = nombre;
+        this.id = id;
+    }
+
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+}
